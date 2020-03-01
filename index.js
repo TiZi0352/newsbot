b4a8a83fef417afce7a6b39eff5bae4172c6f34a
TiZi0352 <taras.zoshchuk@gmail.com>
2020-03-01 03:45:51 +0200

    add site parsing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const sqlite = require('sqlite-sync'); //requiring
+const needle = require('needle');
 
 const token = '1133105459:AAEZH2ATVdqGE7klNisHkEmt1u5tNk7HkDM';
 const bot = new TelegramBot(token, { polling: true });
@@ -31,32 +32,39 @@ sqlite.run(`CREATE TABLE IF NOT EXISTS messages(
 // console.log(sqlite.run("SELECT * FROM messages"));
 
 
-bot.onText(/\/get ([^;'\"])/, (msg, match) => {
+// bot.onText(/\/get ([^;'\"])/, (msg, match) => {
+bot.onText(/\/get (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
     const key = match[1];
     const message = getMessage(key);
 
     if (message.exists) {
-        bot.forwardMessage(chatId, message.fromId, message.messageId);
+        bot.sendMessage(chatId, "Як справи?");
     }
 });
 
 //1.   /add hi
 //2.   [gif, audio, text, sticker]
-bot.onText(/\/add ([^;'\"])/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const key = match[1];
-    const message = getMessage(key);
-
-    if (message.exists) {
-        bot.forwardMessage(chatId, message.fromId, message.messageId);
-    }
-});
-
-// bot.on('message', (msg) => {
+// bot.onText(/\/add ([^;'\"])/, (msg, match) => {
 //     const chatId = msg.chat.id;
-//     bot.sendMessage(chatId, JSON.stringify(msg));
+//     const key = match[1];
+//     const message = getMessage(key);
+
+//     if (message.exists) {
+//         bot.forwardMessage(chatId, message.fromId, message.messageId);
+//     }
 // });
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    if (msg.text == "/start")
+        bot.sendMessage(chatId, "Здоров");
+    else if (msg.text == "/news")
+        sendInterfaxNews(chatId);
+    else
+        bot.sendMessage(chatId, "https://www.rbc.ua/ukr/news/kolichestvo-smertey-koronavirusa-mire-priblizhaetsya-1583019405.html");
+});
 
 function getMessage(key) {
     const data = sqlite.run("SELECT * FROM messages where `key` = ? LIMIT 1", [key]);
@@ -67,3 +75,27 @@ function getMessage(key) {
     data[0].exists = true;
     return data[0];
 };
+
+function sendInterfaxNews(chatId) {
+    var URL = 'https://interfax.com.ua/news/latest.html';
+
+    needle.get(URL, function (err, res) {
+        if (err) throw err;
+
+        var start = '<div class="articles-section-view">';
+        var end = '<div class="pager">';
+
+        var body = res.body.substring(res.body.indexOf(start) + start.length, res.body.indexOf(end));
+
+        var arr = body.split('<div class="grid article">');
+        var textStart = '<a class="article-link"';
+        var hrefText = 'href="';
+
+        for (var i = 0; i < arr.length; ++i) {
+            var text = arr[i].substring(arr[i].indexOf(textStart) + textStart.length);
+            hrefUrl = text.substring(text.indexOf(hrefText) + hrefText.length, text.indexOf('">'));
+            if (text)
+                bot.sendMessage(chatId, "​​​​​​​​​https://interfax.com.ua" + hrefUrl);
+        }
+    });
+}
